0a2c99a8289f26939ab71e6b138990bb18ce9248
Sergio <sergiohds_29@hotmail.com>
2024-12-16 19:23:35 -0700

    it works

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,20 +1,18 @@
-const router = require('express').Router();
-const controller = require('../controllers/baseController.js');
-const documentsRouter = require('./documents.js'); // Assuming documents.js contains your document routes
+const { get } = require('mongoose');
+const mongodb = require('../database/connect.js');
+const { MongoClient, ObjectId } = require('mongodb');
 
-// GET routes -----------------------------------
-router.get('/', controller.getHomepage);
+//-----------------------------------------------------------------------
 
-// Route for /documents
-router.use('/documents', (req, res, next) => {
+const getHomepage = async (req, res, next) => {
+    let message = "Please Log in";
+    let desiredState = "login";
     if (req.oidc.isAuthenticated()) {
-        // If authenticated, proceed to the documents route handler
-        return next(); // Pass control to the documents router
-    } else {
-        // If not authenticated, render blocked page
-        return res.status(401).render('./blockedPage.ejs');
-    }
-}, documentsRouter);  // Use the documentsRouter here
-
-// EXPORT -----------------------------------------
-module.exports = router;
+        message = "Welcome back! You are authenticated!";
+        desiredState = "logout"
+    } 
+    res.status(200).render('./homepage.ejs', {message, desiredState} );
+  };
+  
+  // EXPORT ---------------------------------------------------------------
+  module.exports = {getHomepage};
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
 const router = require('express').Router();
-controller = require('../controllers/baseController.js');
+const controller = require('../controllers/baseController.js');
+const documentsRouter = require('./documents.js'); // Import the documents router
 
 // GET routes -----------------------------------
-router.get('/', controller.getHomepage); 
-router.use('/documents', (req, res) => { 
-        if (req.oidc.isAuthenticated()) {
-            require('./documents.js');
-        } else {
-            res.status(401).render('./blockedPage.ejs');
-        }
-    });
+router.get('/', controller.getHomepage);
+
+router.use('/documents', (req, res, next) => {
+    if (req.oidc.isAuthenticated()) {
+        documentsRouter(req, res, next); // Pass the request to the documents router
+    } else {
+        res.status(401).render('./blockedPage.ejs'); // Render the blocked page for unauthenticated users
+    }
+});
 
 // EXPORT -----------------------------------------
 module.exports = router;
